1942f188b68bb2263003ef89c3cb154ae5548659
Manooonlb <[manonlambert.lb@gmail.com](mailto:manonlambert.lb@gmail.com)>
2023-04-09 14:07:23 +0200

    Time to global!

--- config/twig.php
+++ config/twig.php
@@ -7,5 +7,10 @@ use Twig\Loader\FilesystemLoader;
 $loader = new FilesystemLoader(__DIR__ . '/../src/View'); // Utiliser un chemin relatif pour charger le dossier contenant les templates
 $twig = new Environment($loader); // Créer une instance de Twig
 
+//création de la globale pour le footer
+$contactEmail= "manonlambert.lb@gmail.com";
+$twig->addGlobal('contactEmail',$contactEmail);
+
+//debug
 $twig->addExtension(new Twig\Extension\DebugExtension());
 
